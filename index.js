cf73c95761e43b8835061bed8e2ff6169f623b15
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-08-27 11:32:12 +0600

    after completed heart & copy button count in js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('connected');
+// console.log('connected');
+
+           
+function getElement(id){
+    const element = document.getElementById(id);
+    return element;
+}
+                 //Heart Count
+    let count = 0;
+const counter = getElement('heart-counter');
+const heartButtons = document.getElementsByClassName('btn-heart');
+
+    for(const heartButton of heartButtons){
+        // console.log(heartButton);
+        heartButton.addEventListener('click', function(){
+            count++;
+            counter.innerText = count;
+        })
+    }
+
+            // Copy Count 
+    let copyCount = 0;
+const copyCounter = getElement('copy-counter');
+const copyButtons = document.getElementsByClassName('btn-copy');
+
+    for(const copyButton of copyButtons){
+        copyButton.addEventListener('click',function(){
+
+        copyCount++;
+        copyCounter.innerText = copyCount; 
+
+        })
+    }
+
+            
